7484e2acdfd20df490b410d6471934af2e6c6a39
John Vundi <johnsonvundy2@gmail.com>
2023-06-13 10:21:31 +0300

    configured how to render accordion component on the screen

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Accordion({items}) {
-    const renderedItems = items.map((item)=>{
+    const [expandedIndex, setExpandedIndex] = useState(1 )
+
+    const renderedItems = items.map((item, index)=>{
+       const isExpanded = index === expandedIndex;
+
         return(
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
