44417ad404a448a260abe7a7b4f45c92481e5782
John Vundi <johnsonvundy2@gmail.com>
2023-06-13 09:30:43 +0300

    worked on the accordion component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import Button from "./components/Button";
-import { GoBell } from "react-icons/go";
+import Accordion from "./components/Accordion";
 
 
 function App() {
-  const handleClick =()=>{
-    console.log('click')
-  }
-  return (
-    <div  className="App">
+  const items = [
+    {
+      id:"isgv",
+      label:"Kings men",
+      content:" owsome stuff"
+    },
+    {
+      id:"ihhdj",
+      label:"Kings men",
+      content:" owsome stuff"
+    },
+    {
+      id:"idgv",
+      label:"Kings men",
+      content:" owsome stuff"     
+    },
+  ]
 
-      <div >
-        <Button className="mb-5" success primary rounded onClick={handleClick}>
-           <GoBell/>
-           hello there
-        </Button>
-      </div>
-      <div>
-        <Button danger outline> really</Button>
-      </div>
-      <div>
-        <Button warning outline> Did it</Button>
-      </div>
-      <div>
-        <Button rounded> talk</Button>
-      </div>
-    </div>
-  );
+
+  return<Accordion items={items}/>
 }
 
 export default App;
