d0a72ff9e9018e93ad2251461e1c105db3420640
John Vundi <johnsonvundy2@gmail.com>
2023-06-13 11:01:24 +0300

    added tailwind styling into accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,20 +1,37 @@
 import React,{useState} from 'react'
+import { GoChevronDown, GoChevronLeft} from "react-icons/go";
 
 function Accordion({items}) {
     const [expandedIndex, setExpandedIndex] = useState(1 )
 
+    const handleClick =(nextIndex)=>{
+        setExpandedIndex(nextIndex)
+    }
+
     const renderedItems = items.map((item, index)=>{
        const isExpanded = index === expandedIndex;
 
+       const icon = <span className='text-2xl'>
+        {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
+       </span>
+
         return(
             <div key={item.id}>
-                <div>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+                <div className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer'
+                onClick={()=>handleClick(index)}>
+                    {item.label}
+                    {icon}
+                    </div>
+                {isExpanded && <div className='border-b p-5'>
+                    {item.content}
+                    </div>}
             </div>
         );
     });
   return (
-    <div>{renderedItems}</div>
+    <div className='border-x border-t rounded'>
+        {renderedItems}
+        </div>
   )
 }
 
