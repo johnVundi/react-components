a4caf28a2aba956b9d8b43587c25a4ba20586820
John Vundi <johnsonvundy2@gmail.com>
2023-06-16 17:34:09 +0300

    added soting icons to the sortable table

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,37 +1,87 @@
 import React, { useState } from 'react'
 import Table from './Table'
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
 
 function SortableTable(props) {
-    const {config} = props;
+    const { config, data } = props;
     const [sortOrder, setSortOrder] = useState(null);
     const [sortBy, setSortBy] = useState(null);
 
-    const handleClick =(label)=>{
-        if(sortOrder === null){
+
+    const handleClick = (label) => {
+        if (sortOrder === null) {
             setSortOrder('asc');
             setSortBy(label);
-        }else if(sortOrder === 'asc'){
+        } else if (sortOrder === 'asc') {
             setSortOrder('desc');
             setSortBy(label);
-        }else if(sortOrder === 'desc'){
+        } else if (sortOrder === 'desc') {
             setSortOrder(null);
+            setSortBy(null);
         }
     };
 
-    const updatedConfig = config.map((column)=>{
-        if(!column.sortValue){
+    const updatedConfig = config.map((column) => {
+        if (!column.sortValue) {
             return column;
         }
-        return{
+        return {
             ...column,
-            header:()=> <th onClick={() => handleClick(column.label)}>{column.label} sortable</th>
+            header: () => <th className='cursor-pointer hover:bg-gray-100' onClick={() => handleClick(column.label)}>
+                <div className='flex items-center'>
+                {getIcons(column.label, sortBy, sortOrder)}
+                {column.label}
+                </div>
+            </th>
         }
     })
+    // Only sort data if sortOder && sortBy are not null
+    // Make a copy of the data prop
+    // Find the correct sortValue function and Use it for sorting
+    let sortedData = data;
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find(column => column.label === sortBy);
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
 
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else {
+                return (valueA - valueB) * reverseOrder
+            }
+        })
 
-  return (
-    <Table {...props} config={updatedConfig} />
-  )
+    }
+
+    return (
+        <div>
+            {sortOrder} - {sortBy}
+            <Table {...props} config={updatedConfig} data={sortedData} />
+        </div>
+
+    )
+}
+
+function getIcons(label, sortBy, sortOder) {
+    if (label !== sortBy) {
+        return <div>
+            <GoChevronUp />
+            <GoChevronDown />
+        </div>
+    }
+
+    if (sortOder === null) {
+        return <div>
+            <GoChevronUp />
+            <GoChevronDown />
+        </div>
+    } else if (sortOder === 'asc') {
+        return <GoChevronUp />
+    } else if (sortOder === 'desc') {
+        return <GoChevronDown />
+    }
 }
 
 export default SortableTable
