1f19a749bd34f3a55f621d13ee8a5ad299b90074
John Vundi <johnsonvundy2@gmail.com>
2023-06-18 13:04:41 +0300

    removed the errors associated with destructing the sortabletable

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,30 +1,16 @@
-import React, { useState } from 'react'
+
 import Table from './Table'
 import { GoChevronDown, GoChevronUp } from "react-icons/go";
+import useSort from '../hooks/useSort';
 
 function SortableTable(props) {
     const { config, data } = props;
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
-
-
-    const handleClick = (label) => {
-        if (sortBy && label !== sortBy) {
-            setSortBy('asc');
-            setSortOrder(label);
-            return;
-        }
-        if (sortOrder === null) {
-            setSortOrder('asc');
-            setSortBy(label);
-        } else if (sortOrder === 'asc') {
-            setSortOrder('desc');
-            setSortBy(label);
-        } else if (sortOrder === 'desc') {
-            setSortOrder(null);
-            setSortBy(null);
-        }
-    };
+    const {
+        sortOrder,
+        sortBy,
+        sortedData,
+        handleClick
+    } = useSort(data,config);
 
     const updatedConfig = config.map((column) => {
         if (!column.sortValue) {
@@ -40,25 +26,7 @@ function SortableTable(props) {
             </th>
         }
     })
-    // Only sort data if sortOder && sortBy are not null
-    // Make a copy of the data prop
-    // Find the correct sortValue function and Use it for sorting
-    let sortedData = data;
-    if (sortOrder && sortBy) {
-        const { sortValue } = config.find(column => column.label === sortBy);
-        sortedData = [...data].sort((a, b) => {
-            const valueA = sortValue(a);
-            const valueB = sortValue(b);
 
-            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-            if (typeof valueA === 'string') {
-                return valueA.localeCompare(valueB) * reverseOrder;
-            } else {
-                return (valueA - valueB) * reverseOrder
-            }
-        })
-
-    }
 
     return (
         <Table {...props} config={updatedConfig} data={sortedData} />
